c5e5f2506c8c43bf259fcf64919c6f89026b19ce
Mikkel P. Kristiansen <mikkel.p.kristiansen@gmail.com>
2018-12-05 18:16:06 +0100

    Removed vue logo and added background image for home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,37 @@
 <template>
-  <div class="banner">
-    <h1>To Do App</h1>
-    <h2>Make your life easier</h2>
+  <div class="banner fullscreen">
+    <div class="center-align">
+      <h1>My To-do App</h1>
+      <div class="divider"></div>
+      <h5>Create to-do lists with multiple tasks.</h5>
+      <br />
+      <router-link class="btn blue darken-1 center-text" to="/todos">Let's get started!</router-link>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "home"  
+  name: "home",
+  methods: {}
 };
 </script>
 
 <style scoped>
-
+.fullscreen {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-image: url("../assets/coffee-cup-desk-pen.jpg");
+  background-position: center;
+  background-size: cover;
+}
 </style>
